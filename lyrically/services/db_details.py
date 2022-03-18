3f4e2e9e6e66edbf057cd912857284c095c43513
Robin Svahn <robin.svahn@hotmail.com>
2022-03-18 02:39:59 +0100

    removed commented code

--- lyrically/services/db_details.py
+++ lyrically/services/db_details.py
@@ -3,11 +3,9 @@ import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# print("Version is: " + sqlalchemy.__version__)
-
-# engine = create_engine('sqlite:///:memory:', echo=True)
 engine = create_engine(
     "mssql+pyodbc://localhost\\SQLEXPRESS/python_test?trusted_connection=yes&driver=ODBC+Driver+17+for+SQL+Server", echo=False)
+    
 Session = sessionmaker(bind=engine)
 
 Base = declarative_base()
